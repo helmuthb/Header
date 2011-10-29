ff9273bc9bcf6ed9e0f72af8c18dbacc067d0259
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2011-10-29 14:24:29 +0200

    added first version of extension code

---
+++ src/address.js
@@ -0,0 +1,27 @@
+var latestAddress = "";
+var showAddress = function() {
+  if (latestAddress != document.title) {
+    latestAddress = document.title;
+    var newHtml = document.getElementById("address-info-chrome");
+    if (newHtml == undefined) {
+      newHtml = document.createElement("div");
+      newHtml.id = "address-info-chrome";
+      newHtml.onmouseover = function() {
+        newHtml.style.zIndex = -1000;
+        window.setTimeout(function() {
+          newHtml.style.zIndex = 1000;
+        }, 5000);
+      };
+    }
+    var newText = newHtml.firstChild;
+    if (newText == undefined) {
+      newText = document.createTextNode();
+      newHtml.appendChild(newText);
+    }
+    newText.nodeValue = latestAddress;
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(newHtml);
+  };
+};
+showAddress();
+window.setInterval(showAddress, 100);
