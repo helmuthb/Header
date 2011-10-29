2a23b36752a7f9fdbc3cc0d315ddfdf1e4be67ae
OscarS <Oscar.Sommerer@gmail.at>
2011-10-29 16:53:37 +0200

    Added transperancy option

--- src/writesettings.js
+++ src/writesettings.js
@@ -8,11 +8,14 @@ function getSettings()
 {
 	var align = localStorage.getItem("align");
 	var color = localStorage.getItem("color");
+	var transp = localStorage.getItem("transp");
 	
 	var alignSelect = document.getElementById("Align");
 	var colorSelect = document.getElementById("Color");
+	var transpSelect = document.getElementById("Transp");
 	
 	alignSelect.value = align;
 	colorSelect.value = color;
+	transpSelect.value = transp;
 }
 
